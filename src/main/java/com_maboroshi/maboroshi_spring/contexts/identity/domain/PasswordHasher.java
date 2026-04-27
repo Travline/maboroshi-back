9c1c64313e2132cc10ca4583b632f95628c40e15
Travline <poluxmorales1@gmail.com>
2026-04-26 20:14:12 -0500

    Adding password hasher contract

--- src/main/java/com_maboroshi/maboroshi_spring/contexts/identity/domain/PasswordHasher.java
+++ src/main/java/com_maboroshi/maboroshi_spring/contexts/identity/domain/PasswordHasher.java
@@ -1,9 +1,7 @@
 package com_maboroshi.maboroshi_spring.contexts.identity.domain;
 
-import java.util.Optional;
-
 public interface PasswordHasher {
-  public Optional<String> hash(String pwd);
+  public String hash(String pwd);
 
   public Boolean compare(String plain, String hash);
 }
