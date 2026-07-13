48b2bdf862785c0e6a5bca3dfce091662949b7de
Polux Morales <poluxmorales1@gmail.com>
2026-07-13 16:16:28 -0500

    fix

--- src/main/java/com_maboroshi/spring/contexts/identity/domain/ports/UserRepository.java
+++ src/main/java/com_maboroshi/spring/contexts/identity/domain/ports/UserRepository.java
@@ -5,6 +5,8 @@ import com_maboroshi.spring.contexts.identity.domain.entities.UserMail;
 import com_maboroshi.spring.shared.core.Result;
 import com_maboroshi.spring.shared.errors.RepositoryError;
 
+import java.util.UUID;
+
 public interface UserRepository {
   public Result<User, RepositoryError> save(User newUser);
 
